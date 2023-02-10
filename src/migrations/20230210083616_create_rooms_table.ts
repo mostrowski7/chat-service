09ffe27195a956f9467cd699777a257e69162aa0
mostrowski7 <ursusenszaman@gmail.com>
2023-02-10 19:00:43 +0100

    feat(events): add messages and rooms table migration

---
+++ src/migrations/20230210083616_create_rooms_table.ts
@@ -0,0 +1,18 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.raw(`
+        CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+        CREATE TABLE rooms (
+            id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+            user_id UUID NOT NULL,
+            created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+        )
+    `);
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.raw(`
+        DROP TABLE rooms
+    `);
+}
