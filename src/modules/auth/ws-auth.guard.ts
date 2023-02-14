37a495292e170148e7af52a4746ccbcef159a388
mostrowski7 <ursusenszaman@gmail.com>
2023-02-14 10:43:53 +0100

    fix(events): rename JwtAuthGuard to WsAuthGuard

--- src/modules/auth/ws-auth.guard.ts
+++ src/modules/auth/ws-auth.guard.ts
@@ -3,16 +3,14 @@ import { Socket } from 'socket.io/dist/socket';
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class JwtAuthGuard implements CanActivate {
+export class WsAuthGuard implements CanActivate {
   constructor(private readonly authService: AuthService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const client = context.switchToWs().getClient<Socket>();
     const accessToken = client.handshake.auth?.accessToken;
 
-    if (!this.authService.verifyAccessToken(accessToken)) {
-      return false;
-    }
+    if (!this.authService.verifyAccessToken(accessToken)) return false;
 
     return true;
   }
